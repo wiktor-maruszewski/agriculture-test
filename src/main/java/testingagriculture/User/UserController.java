d1bf38c3f75bd3a9df928784d87f72bdbd945ba9
wiktor-maruszewski <wiktor.maruszewski@student.uwm.edu.pl>
2023-04-21 04:50:12 +0200

    commit

--- src/main/java/testingagriculture/User/UserController.java
+++ src/main/java/testingagriculture/User/UserController.java
@@ -3,19 +3,20 @@ package testingagriculture.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(path="")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("")
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
+    @GetMapping(path = "/users", produces = "application/json")
+    @ResponseBody
+    public List<User> getUsers() {
+        return userService.findAllUsers();
     }
 }
--- src/main/java/testingagriculture/User/UserRepository.java
+++ src/main/java/testingagriculture/User/UserRepository.java
@@ -1,7 +1,9 @@
 package testingagriculture.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     // metody definiujące dostęp do bazy danych
 }
