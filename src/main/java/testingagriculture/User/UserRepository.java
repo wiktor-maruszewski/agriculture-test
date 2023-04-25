14f5e6dc7e8b22f37a1edde21d7a4e7d383e81c0
wiktor-maruszewski <wiktor.maruszewski@student.uwm.edu.pl>
2023-04-25 23:50:00 +0200

    commit

--- src/main/java/testingagriculture/User/UserRepository.java
+++ src/main/java/testingagriculture/User/UserRepository.java
@@ -2,6 +2,8 @@ package testingagriculture.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -10,4 +12,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
+    public List<User> findAll();
 }
--- src/main/java/testingagriculture/User/UserService.java
+++ src/main/java/testingagriculture/User/UserService.java
@@ -1,7 +1,9 @@
 package testingagriculture.User;
 
-
+import org.springframework.stereotype.Service;
 import jakarta.persistence.EntityNotFoundException;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -13,9 +15,6 @@ public class UserService {
     }
 
     public User createUser(User user) {
-        if (userRepository.findByUsername(user.getUsername()).isPresent()) {
-            throw new EntityAlreadyExistsException("User with username " + user.getUsername() + " already exists");
-        }
         return userRepository.save(user);
     }
 
@@ -28,33 +27,9 @@ public class UserService {
         return userRepository.findAll();
     }
 
-    public User updateUser(Long id, User user) {
-        User existingUser = getUserById(id);
-        existingUser.setUsername(user.getUsername());
-        existingUser.setFirstName(user.getFirstName());
-        existingUser.setLastName(user.getLastName());
-        existingUser.setEmail(user.getEmail());
-        existingUser.setPassword(user.getPassword());
-        return userRepository.save(existingUser);
-    }
 
     public void deleteUser(Long id) {
         userRepository.deleteById(id);
     }
 
-    public List<User> searchUsersByUsername(String username) {
-        return userRepository.findByUsernameContainingIgnoreCase(username);
-    }
-
-    public List<User> searchUsersByFirstName(String firstName) {
-        return userRepository.findByFirstNameContainingIgnoreCase(firstName);
-    }
-
-    public List<User> searchUsersByLastName(String lastName) {
-        return userRepository.findByLastNameContainingIgnoreCase(lastName);
-    }
-
-    public List<User> searchUsersByEmail(String email) {
-        return userRepository.findByEmailContainingIgnoreCase(email);
-    }
 }
